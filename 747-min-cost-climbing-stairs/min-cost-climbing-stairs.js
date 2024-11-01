bd964f2aec4909c654128d0e6b19f94eff7c1c33
Davit_Gelovani <davit.gelovani406@ens.tsu.edu.ge>
2024-11-01 23:35:45 +0400

    Time: 2 ms (59.59%) | Memory: 50.6 MB (49.81%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -5,9 +5,10 @@
 var minCostClimbingStairs = function (cost) {
     var n = cost.length;
     var dp = [];
-    for (var i = 0; i < n; i++) {
-        if (i < 2) dp[i] = cost[i];
-        else dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+    for (var i = 2; i < n; i++) {
+        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
     }
     return Math.min(dp[n - 1], dp[n - 2]);
 };
