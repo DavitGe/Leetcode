1047b69c35fe86efabede7d12f6add40712761d0
Davit_Gelovani <davit.gelovani406@ens.tsu.edu.ge>
2024-11-04 13:59:50 +0400

    Time: 9 ms (79.55%) | Memory: 54.1 MB (25.25%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var deleteAndEarn = function(nums) {
+    let arr=[]
+    for(let x of nums){
+        if(arr[x])arr[x]++
+        else arr[x]=1
+    }
+    // console.log(arr,arr[0])
+    let al=arr.length
+    let money=[]
+    for(let i=0;i<al;i++){
+        if(!arr[i]){
+            money[i]=0
+        }
+        else{
+            money[i]=i*arr[i]
+        }
+    }
+    // console.log(money)
+    if(money[1]<money[0])money[1]=money[0]
+    for(let i=2;i<al;i++){
+        if(money[i]+money[i-2]>money[i-1]){
+            money[i]+=money[i-2]
+        }
+        else{
+            money[i]=money[i-1]
+        }
+    }
+    return money.pop()
+};
