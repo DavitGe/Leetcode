6755bbd23ae575e51e7626a58bf2d55f2ef628c9
Davit_Gelovani <davit.gelovani406@ens.tsu.edu.ge>
2024-12-21 03:12:20 -0800

    Time: 65 ms (21.55%) | Memory: 51.6 MB (83.28%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function (arr) {
+    let increased = false;
+    let isIncreasing = true;
+    const len = arr.length;
+    if (len <= 2) {
+        return false;
+    }
+
+    for (let i = 1; i < len; i++) {
+        if(arr[i - 1] == arr[i]){
+            return false
+        }
+        if(isIncreasing){
+            if(arr[i - 1] > arr[i]){
+                if(increased == false){
+                    return false
+                }
+                isIncreasing = false;
+                continue;
+            }else{
+                increased = true;
+            }
+        } else if (arr[i - 1] < arr[i]) {
+            return false;
+        }
+    }
+    return !isIncreasing;
+};
